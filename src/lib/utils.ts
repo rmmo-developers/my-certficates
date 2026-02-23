c1111f9df45086acf634a218d483e83558580bfb
rmmo-developers <kvgonzales.roxasianmoments@gmail.com>
2026-02-23 14:58:21 +0800

    Add files via upload

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,3 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
 
 /**
  * Generates a Unique Certificate ID based on the Manual Date Issued.
